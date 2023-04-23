95639645505a54f0fdb662e07209ddd6abc7bce3
picker807 <60443210+picker807@users.noreply.github.com>
2023-04-23 16:26:33 -0400

    Modified save to local storage

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -10,10 +10,6 @@ export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
-// here I have added code to fetch the current Local Storage data, put it in an Array
-  // and add the new data (or cart item) to the saved data. Checks first to see if the current data is already an array, then uses that or puts it in an array. This creates an Array of objects.
-  // The result is that 1) more than one item can be saved in the cart and 2) the code in cart.js that
-  // retreives the saved data works without a problem. Whereas before it could not run map() on an object.
   export function setLocalStorage(key, data) {
     let currentData = JSON.parse(localStorage.getItem(key)) || [];
     currentData = Array.isArray(currentData) ? currentData : [currentData];
