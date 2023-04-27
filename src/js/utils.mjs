7302665756f492aec76243d0a5d6983a97216bf2
picker807 <60443210+picker807@users.noreply.github.com>
2023-04-26 21:24:19 -0400

    Major changes for the Wk2 Team activity
    Changed products to find by ID. Major changes throughout JS files to make this work.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -10,12 +10,21 @@ export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
-  export function setLocalStorage(key, data) {
-    let currentData = JSON.parse(localStorage.getItem(key)) || [];
-    currentData = Array.isArray(currentData) ? currentData : [currentData];
-    const newData = [...currentData, data];
-    localStorage.setItem(key, JSON.stringify(newData));
-  }
+export function setLocalStorage(key, data) {
+  let currentData = JSON.parse(localStorage.getItem(key)) || [];
+  currentData = Array.isArray(currentData) ? currentData : [currentData];
+  const newData = [...currentData, data];
+  localStorage.setItem(key, JSON.stringify(newData));
+}
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
