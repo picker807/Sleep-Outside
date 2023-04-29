4d99d45590d38fc0ffb600ad8d9cae9b600e118d
picker807 <60443210+picker807@users.noreply.github.com>
2023-04-29 17:50:54 -0400

    Cleaned up files after merge
    Many JS files were moved into public. Public should only contain image and JSON files.

--- src/public/js/main.js
+++
@@ -1,5 +0,0 @@
-import { ProductData } from "./ProductData.mjs"
-import { ProductListing } from "./ProductList.mjs"
-
-const productData = new ProductData();
-const productList = new ProductListing();
