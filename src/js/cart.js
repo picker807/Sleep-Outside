69cc8e594b04b9aad66e44f5d41bf5eab8d0850c
picker807 <60443210+picker807@users.noreply.github.com>
2023-04-29 20:11:07 -0400

    Added cart total

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,36 @@
 import { getLocalStorage } from "./utils.mjs";
 
+//Make the cart total visible when cart has items.
+function makeFooterVisible(cartFooter, numItems) {
+  if (numItems){
+      cartFooter.style.display = "inline";
+  } else {
+      cartFooter.style.display = "none";
+  }
+}
+//Calculates the total for items and cost
+function getTotal(cartItems){
+  let totalCost = 0;
+  let numItems = 0;
+  for (let i = 0; i <cartItems.length; i++) {
+    const item = cartItems[i];
+    const finalCost = item.FinalPrice;
+    totalCost += finalCost;
+    numItems += 1
+  }
+  renderTotal(numItems, totalCost); 
+}
+//Renders the cart total cost and item count
+function renderTotal(numItems, totalCost) {
+  const cartFooter = document.querySelector(".cart-footer");
+  cartFooter.querySelector(".cart-items").innerHTML += numItems;
+  cartFooter.querySelector(".cart-total").innerHTML += "$" + totalCost;
+  makeFooterVisible(cartFooter, numItems);
+}
+//Renders the cart contents
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  getTotal(cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
